f9482ff82e66058a35156abacd6d5b1996855fa3
awinogradov <winogradovaa@gmail.com>
2017-11-13 15:02:49 +0300

    up to date

--- renderToString/index.js
+++ renderToString/index.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const join = require('path').join;
 
-const CHILDREN_NUM = 200;
+const CHILDREN_NUM = 5000;
 const templates = fs.readFileSync(join(
   process.cwd(), 'renderToString', 'react-xjst', 'templates', 'templates.bemhtml.js'),
   'utf-8'
@@ -11,7 +11,7 @@ const templates = fs.readFileSync(join(
 
 module.exports = {
   'react': require('./react')(CHILDREN_NUM),
-  'react-xjst': require('./react-xjst')(CHILDREN_NUM),
+  // 'react-xjst': require('./react-xjst')(CHILDREN_NUM),
   'bem-xjst': require('./bem-xjst')(CHILDREN_NUM, templates),
-  'xjst-ddsl': require('./xjst-ddsl')(CHILDREN_NUM, templates)
+  // 'xjst-ddsl': require('./xjst-ddsl')(CHILDREN_NUM, templates)
 };
